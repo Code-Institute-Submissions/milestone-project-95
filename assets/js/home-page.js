0b13d2c841585e017939498f16ef528a8eb28533
JohnGC1510 <johngc1510@gmail.com>
2020-08-18 15:24:33 +0000

    Added the about information boxes with filler text and added appropriate JS to toogle them to slide up and down when you click on the header of the box

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,1 +1,5 @@
-console.log("hi");
+$(".info-title-container").click(function(){
+    $(this).siblings().slideToggle();
+    $(this).children().children("i").toggleClass("fa-angle-double-down").toggleClass("fa-angle-double-up");
+   
+});
