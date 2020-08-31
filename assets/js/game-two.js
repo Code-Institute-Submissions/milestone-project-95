01071caa0d4f3893c5ded3b54bbc9406b184e7fd
JohnGC1510 <johngc1510@gmail.com>
2020-08-31 12:02:34 +0000

    added click functions to highlight the buttons and to recall old buttons pressed and highlight them

--- assets/js/game-two.js
+++ assets/js/game-two.js
@@ -1,22 +1,61 @@
-let colors = ["red","blue","green","cyan","orange"];
-let click,color;
+let colors = [];
+let click;
 let buttons = $("div.game-button");
+let game = false;
+let level = 0;
+let win = 0;
+let match = 0;
 
-
-let selectButton = Math.floor(Math.random()*colors.length);
-buttons[selectButton].classList.add("highlight");
-setTimeout(function(){
+function highlightNewButton() {
+  let selectButton = Math.floor(Math.random() * 5);
+  buttons[selectButton].classList.add("highlight");
+  setTimeout(function () {
     buttons[selectButton].classList.remove("highlight");
-},1000);
- 
-localStorage.color = buttons[selectButton].getAttribute("data-color");;
-
-function check(){
-    colorCheck = localStorage.color;
-    click = $(this).data("color");
-    if(click === colorCheck){
-        console.log("Win");
-    }
+  }, 1000);
+  colors.push(buttons[selectButton].getAttribute("data-color"));
 }
 
-$("game-button").on("click",check);
+function highlightOldButtons() {
+  for (let i = 0; i < colors.length; i++) {
+    let color = colors[i];
+    console.log(color);
+    $(`div#${color}-button`).addClass("highlight");
+    setTimeout(function () {
+      $(`div#${color}-button`).removeClass("highlight");
+    }, 1000);
+  }
+}
+function startLevel() {
+  level++;
+  game = true;
+  setTimeout(highlightNewButton, 1000);
+}
+
+function resetGame() {}
+
+function check() {
+  startLevel();
+  while (game === true && level > win) {
+    for (i = 0; i < level; i++) {
+      colorCheck = colors[i];
+      click = $(this).data("color");
+      if (click === colorCheck) {
+        console.log("Win");
+        match++;
+        if(match < level){
+            setTimeout(highlightOldButtons,1000);
+        }
+        if (match === level) {
+          win = match;
+          match = 0;
+        }
+      } else {
+        console.log("Lose");
+        game = false;
+        colors = [];
+      }
+    }
+  }
+}
+setTimeout(highlightNewButton, 1000);
+$(".game-button").on("click", check);
