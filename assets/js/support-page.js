ea1ad90e9c4dc5202c9c296f95040333b99f43e0
JohnGC1510 <johngc1510@gmail.com>
2020-09-01 21:38:05 +0000

    Added useful lcoations of support to google maps and centered mpa on UK

--- assets/js/support-page.js
+++ assets/js/support-page.js
@@ -1,39 +1,103 @@
-$(".support-heading").on("click", function(){
-    
-    $(".support-heading").siblings().addClass("hidden");
-    $(this).siblings().removeClass("hidden");
-})
+$(".support-heading").on("click", function () {
+  $(".support-heading").siblings().addClass("hidden");
+  $(this).siblings().removeClass("hidden");
+});
 
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: 46.619261,
-                    lng: -33.134766
-                }
-            });
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5,
+    center: {
+      lat: 53.073396,
+      lng: -1.664170,
+    },
+  });
 
-            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+  var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
-            var locations = [{
-                lat: 40.785091,
-                lng: -73.968285
-            }, {
-                lat: 41.084045,
-                lng: -73.874256
-            }, {
-                lat: 40.754932,
-                lng: -73.984016
-            }];
+  var locations = [
+    {
+      lat: 51.500900,
+      lng: -0.116878,
+    },
+    {
+      lat: 51.506029,
+      lng: -0.085206,
+    },
+    {
+      lat: 51.516766,
+      lng: -0.099711,
+    },
+    {
+      lat: 55.863731,
+      lng: -4.337989,
+    },
+    {
+      lat: 55.871811,
+      lng: -4.323421,
+    },
+    {
+      lat: 55.881806,
+      lng: -4.313970,
+    },
+    {
+      lat: 55.973937,
+      lng: -3.176686,
+    },
+    {
+      lat: 55.973888,
+      lng: -3.196599,
+    },
+    {
+      lat: 55.961913,
+      lng: -3.230845,
+    },
+     {
+      lat: 53.387708,
+      lng: -2.292388,
+    },
+    {
+      lat: 53.399831,
+      lng: -2.302511,
+    },
+    {
+      lat: 53.415139,
+      lng: -2.314184,
+    },
+        {
+      lat: 51.478771,
+      lng: -3.179306,
+    },
+    {
+      lat: 51.468671,
+      lng: -3.169205,
+    },
+    {
+      lat: 51.436909,
+      lng: -3.180177,
+    },
+    {
+      lat: 52.480341,
+      lng: -1.839631,
+    },
+    {
+      lat: 52.517371,
+      lng: -1.838631,
+    },
+    {
+      lat: 52.432422,
+      lng: -1.821847,
+    }
+  ];
 
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
+  var markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
 
-            var markerCluster = new MarkerClusterer(map, markers, {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
-        }
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
